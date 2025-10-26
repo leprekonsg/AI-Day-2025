b79ccff55e85b08f4f7adde782adfe5b841d5bdf
Wei Song <9055584+leprekonsg@users.noreply.github.com>
2025-10-27 00:18:41 +0800

    update server.js again

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// This tells the server to serve all the static files (html, css, js, img)
-// from the root directory of your project.
+// This single line is all we need.
+// It correctly serves index.html for the root path ('/')
+// and all other files like moderator.html, presenter.html, and all CSS/JS files.
 app.use(express.static(path.join(__dirname, '/')));
 
-// MODIFIED: Replaced the old '*' syntax with the modern '/*' syntax for a catch-all route.
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+// The problematic app.get('/*', ...) route has been completely removed.
 
 // Azure App Service provides the port number via an environment variable.
-// We use that port, or a default (8080) for local testing.
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
